57bea479d907370a5f9d64d8f5e8ef77e5626a5d
Jason von Nieda <jason@vonnieda.org>
2023-03-17 18:38:45 -0500

    It works! Took me 3 days to learn enough Rust to figure out how to use Arc to reference the shared image library. Sheesh.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,103 @@
-mod search_bar;
-// mod card_grid;
+use eframe;
+use eframe::egui::{self, Grid, ImageButton, Link, Response, ScrollArea, TextEdit, Ui};
+use eframe::epaint::{FontFamily, FontId};
 
-use eframe::egui;
-use search_bar::SearchBar;
+mod music_library;
 
-fn main() -> Result<(), eframe::Error> {
-    // Log to stdout (if you run with `RUST_LOG=debug`).
-    tracing_subscriber::fmt::init();
+use music_library::*;
 
-    let options = eframe::NativeOptions {
+// TODO go through https://github.com/catppuccin/egui/blob/main/examples/todo.rs
+// for ideas.
+// TODO make grid full width
+// TODO make number of columns adapt to window width and tile size
+// TODO tile size slider
+
+fn main() {    
+    let native_options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(1440.0, 1024.0)),
         ..Default::default()
     };
-    eframe::run_native("Music", options, Box::new(|_cc| Box::new(App::default())))
+    eframe::run_native(
+        "Music",
+        native_options,
+        Box::new(|_cc| Box::new(App::default())),
+    )
+    .expect("eframe: pardon me, but no thank you");
 }
 
 struct App {
+    query_string: String,
+    music_library: MusicLibrary,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
+            query_string: String::from(""),
+            music_library: MusicLibrary::example(),
         }
     }
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // ui.add(&mut SearchBar::default())
+            catppuccin_egui::set_theme(ctx, catppuccin_egui::FRAPPE);
+            egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical(|ui| {
+                search_bar(&mut self.query_string, ui);
+                ui.add_space(16.0);
+                release_grid(&self.music_library.releases, ctx, ui);
+            });
         });
     }
+}
+
+fn search_bar(query_string: &mut String, ui: &mut Ui) -> Response {
+    ui.horizontal(|ui| {
+        ui.add(
+            TextEdit::singleline(query_string)
+                .hint_text("What sounds good?")
+                // TODO how do I use the theme font? Or the default, more specifically.
+                .font(FontId::new(32.0, FontFamily::Proportional))
+                .desired_width(f32::INFINITY),
+        );
+    })
+    .response
+}
+
+fn release_grid(releases: &Vec<Release>, ctx: &egui::Context, ui: &mut Ui) {
+    let num_columns = 6;
+    // TODO use show_rows to improve performance. I 
+    // tried previously and I couldn't get the rendering right.
+    ScrollArea::vertical().show(ui, |ui| {
+        Grid::new("release_grid")
+            .num_columns(num_columns)
+            .spacing(egui::vec2(16.0, 16.0))
+            .show(ui, |ui| {
+                for (i, release) in releases.iter().enumerate() {
+                    release_card(&release, ctx, ui);
+                    if i % num_columns == num_columns - 1 {
+                        ui.end_row();
+                    }
+                }
+            });
+    });
+}
+
+fn release_card(release: &Release, ctx: &egui::Context, ui: &mut Ui) -> Response {
+    ui.vertical(|ui| {
+        if let Some(cover_image) = &release.cover_image {
+            ui.add(ImageButton::new(cover_image.texture_id(ctx), egui::vec2(200.0, 200.0)));
+        }
+        // else {
+            // TODO default image
+        //     ui.add()
+        // }
+        ui.add_space(8.0);
+        ui.add(Link::new(&release.title));
+        ui.add_space(2.0);
+        ui.add(Link::new(&release.artist));
+    })
+    .response
 }
 
---
+++ src/music_library.rs
@@ -0,0 +1,166 @@
+use egui_extras::RetainedImage;
+use rand::prelude::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::sync::Arc;
+use rand::seq::SliceRandom;
+
+#[derive(Default)]
+pub struct MusicLibrary {
+    pub releases: Vec<Release>,
+    pub playlists: Vec<Playlist>,
+    pub images: Vec<Arc<RetainedImage>>,
+}
+
+#[derive(Default)]
+pub struct Playlist {
+    pub name: String,
+    pub items: Vec<Release>,
+    pub cover_image: Option<Arc<RetainedImage>>,
+}
+
+#[derive(Default)]
+pub struct Release {
+    pub title: String,
+    pub artist: String,
+    pub release_year: u32,
+    pub cover_image: Option<Arc<RetainedImage>>,
+}
+
+#[derive(Default)]
+pub struct Artist {
+    pub name: String,
+    pub cover_image: Option<Arc<RetainedImage>>,
+}
+
+#[derive(Default)]
+pub struct Track {
+    pub title: String,
+    pub lyrics: Lyrics,
+    pub cover_image: Option<Arc<RetainedImage>>,
+}
+
+#[derive(Default)]
+pub struct Lyrics {
+    pub lyrics: String,
+}
+
+impl MusicLibrary {
+    // pub fn from_navidrome(url: &str, username: &str, password: &str) {
+
+    // }
+
+    pub fn example() -> Self {
+        let _num_artists = 706;
+        let num_releases = 1371;
+
+        let mut music_library = Self::default();
+        music_library.images = load_sample_images();
+        music_library.releases = create_sample_releases(num_releases, &music_library);
+
+        return music_library;
+    }
+}
+
+fn load_sample_images() -> Vec<Arc<RetainedImage>> {
+    let filenames = [
+        "art/samples/getCoverArt-0.jpg",
+        "art/samples/getCoverArt-1.jpg",
+        "art/samples/getCoverArt-2.jpg",
+        "art/samples/getCoverArt-3.jpg",
+        "art/samples/getCoverArt-4.jpg",
+        "art/samples/getCoverArt-5.jpg",
+        "art/samples/getCoverArt-6.jpg",
+        "art/samples/getCoverArt-7.jpg",
+        "art/samples/getCoverArt-8.jpg",
+        "art/samples/getCoverArt-9.jpg",
+    ];
+    let mut images = Vec::new();
+    for filename in filenames {
+        if let Ok(image) = load_image(filename) {
+            images.push(Arc::new(image));
+        }
+    }
+    return images;
+}
+
+fn load_image(filename: &str) -> Result<RetainedImage, String> {
+    // TODO handle error
+    let mut f = File::open(filename).unwrap();
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer).unwrap();
+    return RetainedImage::from_image_bytes(
+        filename, 
+        &buffer[..]);
+}
+
+fn create_sample_releases(count: i32, music_library: &MusicLibrary) -> Vec<Release> {
+    let mut releases = Vec::new();
+    for _ in 0..count {
+        releases.push(create_sample_release(music_library));
+    }
+    return releases;
+}
+
+fn create_sample_release(music_library: &MusicLibrary) -> Release {
+    let mut rng = rand::thread_rng();
+    Release {
+        artist: generate_artist_name(),
+        title: generate_album_name(),
+        release_year: 1930 + (rng.gen::<u32>() % 83),
+        cover_image: music_library.images.choose(&mut rng).cloned(),
+    }
+}
+
+fn generate_artist_name() -> String {
+    let artist_words = [
+        "The",
+        "Open",
+        "Wild",
+        "Final",
+        "Last",
+        "Alaska",
+        "Bear",
+        "Beach",
+        "Time",
+        "Animal",
+    ];
+
+    let mut rng = rand::thread_rng();
+    let mut shuffled_words = artist_words.to_vec();
+    shuffled_words.shuffle(&mut rng);
+    
+    let album = shuffled_words[..2].join(" ");
+    return album;
+}
+
+fn generate_album_name() -> String {
+    let album_words = [
+        "A",
+        "Place",
+        "For",
+        "Children",
+        "Walking",
+        "Towards",
+        "Bright",
+        "Signs",
+        "Stars",
+        "Body",
+        "Music",
+        "Songs",
+        "Deaf",
+        "Blind",
+    ];
+
+
+    let mut rng = rand::thread_rng();
+    let mut shuffled_words = album_words.to_vec();
+    shuffled_words.shuffle(&mut rng);
+    
+    let album = shuffled_words[..3].join(" ");
+    return album;
+}
+    
+// fn generate_sample_image() -> Arc<RetainedImage> {
+//     return Arc::new(RetainedImage::from_color_image("debug_name", ColorImage::example()));
+// }
