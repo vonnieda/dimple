709b1c0077e6cbd1b5498c8bdef09cb973722017
Jason von Nieda <jason@vonnieda.org>
2023-07-30 01:22:04 -0500

    Added models for links and image links. This lets me attach a URL to the various linkable bits we'll pass around. Added the basics of navigation. Cleaned up a ton of the .slint files just by sending down a .navigate function. I suspect I could just make this global somehow? Yep, just checked. callbacks and properties. So, I'll move navigate up there and hooray.

--- dimple_ui_slint/src/main.rs
+++ dimple_ui_slint/src/main.rs
@@ -1,10 +1,10 @@
 use std::sync::Arc;
 
-use dimple_core::{library::{LibraryHandle, Library}, model::{HasArtwork, Artist, Release, Genre, Image}};
+use dimple_core::{library::LibraryHandle, model::{HasArtwork, Artist, Release, Genre}};
 use dimple_player::player::Player;
 use dimple_ui_slint::{settings::Settings, librarian::Librarian, image};
-use ::image::{DynamicImage, RgbaImage, RgbImage};
-use slint::{SharedString, ModelRc, SharedPixelBuffer, Rgba8Pixel, Weak, Rgb8Pixel};
+use ::image::DynamicImage;
+use slint::{ModelRc, SharedPixelBuffer, Weak, Rgb8Pixel, SharedString};
 
 slint::include_modules!();
 
@@ -15,173 +15,130 @@ struct DynamicImageCard {
     url: String,
 }
 
-// TODO might try checking this in and then redoing it keeping the cardgrid
-// and card here. It would simplify the code so much to not have to bubble
-// stuff up and down all the time. Worth thinking about.
-
 fn main() -> Result<(), slint::PlatformError> {
     let mut builder = env_logger::Builder::new();
     builder.filter_level(log::LevelFilter::Info);
     builder.format_timestamp_millis();
     builder.parse_default_env();
     builder.init();
-    log::info!("Log initialized.");
+    log::debug!("Log initialized.");
 
-    log::info!("Loading settings.");
+    log::debug!("Loading settings.");
     let settings = Settings::default();
 
-    log::info!("Loading libraries.");
+    log::debug!("Loading libraries.");
     let librarian: Arc<Librarian> = Arc::new(Librarian::from(settings.libraries));
-    let library: LibraryHandle = librarian.clone();
+    let library: LibraryHandle = librarian;
 
-    log::info!("Creating player.");
+    log::debug!("Creating player.");
     let library_1 = library.clone();
     let player = Player::new(library_1);
 
-    log::info!("Initializing UI.");
+    log::debug!("Initializing UI.");
     let ui = AppWindow::new()?;
 
     let library_1 = library.clone();
     let ui_1 = ui.as_weak();
-    ui.on_nav_home(move || {
-        nav_home(library_1.clone(), ui_1.clone());
-    });
-
-    let library_1 = library.clone();
-    let ui_1 = ui.as_weak();
-    ui.on_nav_albums(move || {
-        nav_albums(library_1.clone(), ui_1.clone());
-    });
-
-    let library_1 = library.clone();
-    let ui_1 = ui.as_weak();
-    ui.on_nav_artists(move || {
-        nav_artists(library_1.clone(), ui_1.clone());
-    });
-
-    let library_1 = library.clone();
-    let ui_1 = ui.as_weak();
-    ui.on_nav_genres(move || {
-        nav_genres(library_1.clone(), ui_1.clone());
-    });
-
-    let player_1 = player.clone();
-    ui.on_card_image_clicked(move |model: CardModel| {
-        // player_1.read().unwrap().queue_release(release)
-        println!("{}", model.url);
-    });
-
-    ui.invoke_nav_home();
-
-    // let ui_handle = ui.as_weak();
-    // ui.on_nav_search(move |query: SharedString| {
-    //     let ui = ui_handle.unwrap();
-    // });
-
-    // log::info!("Starting Librarian.");
-    // std::thread::spawn(move || {
-    //     log::info!("Refreshing libraries.");
-    //     librarian.refresh_all_libraries();
-    // });
-
-    // Launch a thread to monitor the player and update the player bar.
-    // TODO Do better.
-    let library_1 = library.clone();
-    let ui_1 = ui.as_weak();    
-    std::thread::spawn(move || {
-        loop {
-
-        }
+    ui.on_navigate(move |url| {
+        ui_1.upgrade_in_event_loop(move |ui| {
+            println!("{}", url);
+            if url.starts_with("dimple://home") {
+                ui.set_page(0);
+            }
+            else {
+                ui.set_page(1);
+            }
+        })?;
     });
 
     log::info!("Running UI.");
     ui.run()
 }
 
-fn nav_home(library: LibraryHandle, ui: Weak<AppWindow>) {
-    nav_artists(library, ui)
-}
+// fn nav_home(library: LibraryHandle, ui: Weak<AppWindow>) {
+//     nav_artists(library, ui)
+// }
 
-fn nav_artists(library: LibraryHandle, ui: Weak<AppWindow>) {
-    std::thread::spawn(move || {
-        log::info!("Getting artists.");
-        let artists = library.artists();
+// fn nav_artists(library: LibraryHandle, ui: Weak<AppWindow>) {
+//     std::thread::spawn(move || {
+//         log::info!("Getting artists.");
+//         let artists = library.artists();
 
-        log::info!("Making artist cards.");
-        let artist_cards: Vec<DynamicImageCard> = artists.iter()
-            .map(|artist| {
-                artist_to_card(artist, library.clone())
-            })
-            .collect();
+//         log::info!("Making artist cards.");
+//         let artist_cards: Vec<DynamicImageCard> = artists.iter()
+//             .map(|artist| {
+//                 artist_to_card(artist, library.clone())
+//             })
+//             .collect();
 
-        log::info!("Sending cards to UI.");
-        ui.upgrade_in_event_loop(move |ui| { 
-                log::info!("Converting artist cards to card models.");
-                let cards: Vec<CardModel> = artist_cards.into_iter()
-                    .map(Into::into)
-                    .collect();
+//         log::info!("Sending cards to UI.");
+//         ui.upgrade_in_event_loop(move |ui| { 
+//                 log::info!("Converting artist cards to card models.");
+//                 let cards: Vec<CardModel> = artist_cards.into_iter()
+//                     .map(Into::into)
+//                     .collect();
 
-                log::info!("Setting cards.");
-                ui.set_cards(ModelRc::from(cards.as_slice())); 
-                log::info!("Done setting cards.");
-            })
-            .unwrap();
-    });
-}
+//                 log::info!("Setting cards.");
+//                 ui.set_cards(ModelRc::from(cards.as_slice())); 
+//                 log::info!("Done setting cards.");
+//             })
+//             .unwrap();
+//     });
+// }
 
-fn nav_albums(library: LibraryHandle, ui: Weak<AppWindow>) {
-    std::thread::spawn(move || {
-        log::info!("Getting albums.");
-        let releases = library.releases();
+// fn nav_albums(library: LibraryHandle, ui: Weak<AppWindow>) {
+//     std::thread::spawn(move || {
+//         log::info!("Getting albums.");
+//         let releases = library.releases();
 
-        log::info!("Making album cards.");
-        let album_cards: Vec<DynamicImageCard> = releases.iter()
-            .map(|release| {
-                release_to_card(&release, library.clone())
-            })
-            .collect();
+//         log::info!("Making album cards.");
+//         let album_cards: Vec<DynamicImageCard> = releases.iter()
+//             .map(|release| {
+//                 release_to_card(&release, library.clone())
+//             })
+//             .collect();
 
-        log::info!("Sending cards to UI.");
-        ui.upgrade_in_event_loop(move |ui| { 
-                log::info!("Converting album cards to card models.");
-                let cards: Vec<CardModel> = album_cards.into_iter()
-                    .map(Into::into)
-                    .collect();
+//         log::info!("Sending cards to UI.");
+//         ui.upgrade_in_event_loop(move |ui| { 
+//                 log::info!("Converting album cards to card models.");
+//                 let cards: Vec<CardModel> = album_cards.into_iter()
+//                     .map(Into::into)
+//                     .collect();
 
-                log::info!("Setting cards.");
-                ui.set_cards(ModelRc::from(cards.as_slice())); 
-                log::info!("Done setting cards.");
-            })
-            .unwrap();
-    });
-}
+//                 log::info!("Setting cards.");
+//                 ui.set_cards(ModelRc::from(cards.as_slice())); 
+//                 log::info!("Done setting cards.");
+//             })
+//             .unwrap();
+//     });
+// }
 
-fn nav_genres(library: LibraryHandle, ui: Weak<AppWindow>) {
-    std::thread::spawn(move || {
-        log::info!("Getting genres.");
-        let objects = library.genres();
+// fn nav_genres(library: LibraryHandle, ui: Weak<AppWindow>) {
+//     std::thread::spawn(move || {
+//         log::info!("Getting genres.");
+//         let objects = library.genres();
 
-        log::info!("Making cards.");
-        let cards: Vec<DynamicImageCard> = objects.iter()
-            .map(|o| {
-                genre_to_card(o, library.clone())
-            })
-            .collect();
+//         log::info!("Making cards.");
+//         let cards: Vec<DynamicImageCard> = objects.iter()
+//             .map(|o| {
+//                 genre_to_card(o, library.clone())
+//             })
+//             .collect();
 
-        log::info!("Sending cards to UI.");
-        ui.upgrade_in_event_loop(move |ui| { 
-                log::info!("Converting cards to card models.");
-                let cards: Vec<CardModel> = cards.into_iter()
-                    .map(Into::into)
-                    .collect();
+//         log::info!("Sending cards to UI.");
+//         ui.upgrade_in_event_loop(move |ui| { 
+//                 log::info!("Converting cards to card models.");
+//                 let cards: Vec<CardModel> = cards.into_iter()
+//                     .map(Into::into)
+//                     .collect();
 
-                log::info!("Setting cards.");
-                ui.set_cards(ModelRc::from(cards.as_slice())); 
-                log::info!("Done setting cards.");
-            })
-            .unwrap();
-    });
-}
+//                 log::info!("Setting cards.");
+//                 ui.set_cards(ModelRc::from(cards.as_slice())); 
+//                 log::info!("Done setting cards.");
+//             })
+//             .unwrap();
+//     });
+// }
 
 fn artist_to_card(artist: &Artist, library: LibraryHandle) -> DynamicImageCard {
     // TODO wow, yikes. How many times do we clone / shuffle the
@@ -231,27 +188,27 @@ fn genre_to_card(genre: &Genre, library: LibraryHandle) -> DynamicImageCard {
 //     slint::Image::from_rgba8(shared_pixbuf)
 // }
 
-fn dynamic_image_to_slint_image(dynamic_image: &DynamicImage) -> slint::Image {
-    // TODO may be possible to limit the number of copies here
-    let rgb8_image = dynamic_image.clone().into_rgb8();
-    let shared_pixbuf = SharedPixelBuffer::<Rgb8Pixel>::clone_from_slice(
-        rgb8_image.as_raw(),
-        rgb8_image.width(),
-        rgb8_image.height(),
-    );
-    slint::Image::from_rgb8(shared_pixbuf)
-}
+// fn dynamic_image_to_slint_image(dynamic_image: &DynamicImage) -> slint::Image {
+//     // TODO may be possible to limit the number of copies here
+//     let rgb8_image = dynamic_image.clone().into_rgb8();
+//     let shared_pixbuf = SharedPixelBuffer::<Rgb8Pixel>::clone_from_slice(
+//         rgb8_image.as_raw(),
+//         rgb8_image.width(),
+//         rgb8_image.height(),
+//     );
+//     slint::Image::from_rgb8(shared_pixbuf)
+// }
 
-impl From<DynamicImageCard> for CardModel {
-    fn from(value: DynamicImageCard) -> Self {
-        CardModel {
-            title: value.title.clone().into(),
-            sub_title: value.sub_title.clone().into(),
-            image: dynamic_image_to_slint_image(&value.image),
-            url: value.url.into(),
-        }
-    }
-}
+// impl From<DynamicImageCard> for CardModel {
+//     fn from(value: DynamicImageCard) -> Self {
+//         CardModel {
+//             title: value.title.clone().into(),
+//             sub_title: value.sub_title.clone().into(),
+//             image: dynamic_image_to_slint_image(&value.image),
+//             url: value.url.into(),
+//         }
+//     }
+// }
 
 // /// Load the given image from cache, or from the library and then cache it,
 // /// and return it.
