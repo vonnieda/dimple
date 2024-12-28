95671780a74ebd3d9016f22336bbda73ff40e398
Jason von Nieda <jason@vonnieda.org>
2024-12-28 12:18:47 -0600

    changed downloader to a lru limit 5 to keep it from growing infinitely, fixes issue where app eventually uses all the memory in the world

--- dimple_core/src/player/track_downloader.rs
+++ dimple_core/src/player/track_downloader.rs
@@ -1,5 +1,6 @@
-use std::{collections::HashMap, io::Cursor, sync::{Arc, RwLock}};
+use std::{borrow::Borrow, io::Cursor, num::NonZeroUsize, sync::{Arc, RwLock}};
 
+use lru::LruCache;
 use playback_rs::{Hint, Song};
 use threadpool::ThreadPool;
 
@@ -12,34 +13,36 @@ pub enum TrackDownloadStatus {
     Error(String),
 }
 
-#[derive(Clone, Default)]
+#[derive(Clone)]
 pub struct TrackDownloader {
-    track_key_status: Arc<RwLock<HashMap<String, TrackDownloadStatus>>>,
+    cache: Arc<RwLock<LruCache<String, TrackDownloadStatus>>>,
     threadpool: ThreadPool,
 }
 
-impl TrackDownloader {
-    // TODO limit the cache of ready tracks to N so memory doesn't grow forever.
-    // They can always be reloaded.
-    // Or, this moves to the real cache which is memory + disk based
-    pub fn get(&self, track: &Track, library: &Library) -> TrackDownloadStatus {
-        let track = track.clone();
-        let track_key = track.key.clone().unwrap();
-        let mut track_key_status = self.track_key_status.write().unwrap();
-        match track_key_status.get(&track_key) {
-            Some(status) => status.clone(),
-            None => {
-                track_key_status.insert(track_key.clone(), TrackDownloadStatus::Downloading);
-                let track_key_status = self.track_key_status.clone();
-                let library = library.clone();
-                self.threadpool.execute(move || {
-                    let content = library.load_track_content(&track).expect("No valid sources found.");
-                    let song = Song::new(Box::new(Cursor::new(content)), &Hint::new(), None).unwrap();
-                    track_key_status.write().unwrap().insert(track_key, TrackDownloadStatus::Ready(song));
-                });
-                TrackDownloadStatus::Downloading
-            }
+impl Default for TrackDownloader {
+    fn default() -> Self {
+        Self { 
+            // TODO magic, just felt right, needs to handle multiple downloads in
+            // progress. Probably should be related to threadpool len.
+            cache: Arc::new(RwLock::new(LruCache::new(NonZeroUsize::new(5).unwrap()))), 
+            threadpool: Default::default() 
         }
+    }
+}
+
+impl TrackDownloader {
+    pub fn get(&self, track: &Track, library: &Library) -> TrackDownloadStatus {
+        let cache = self.cache.clone();
+        let track = track.clone();
+        let library = library.clone();
+        self.cache.write().unwrap().get_or_insert(track.key.clone().unwrap(), move || {
+            self.threadpool.execute(move || {
+                let content = library.load_track_content(&track).expect("No valid sources found.");
+                let song = Song::new(Box::new(Cursor::new(content)), &Hint::new(), None).unwrap();
+                cache.write().unwrap().put(track.key.clone().unwrap(), TrackDownloadStatus::Ready(song));
+            });
+            TrackDownloadStatus::Downloading
+        }).clone()
     }
 }
 
